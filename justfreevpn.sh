d0b928706402de24753ce611699c402c235a2b5d
Bruce Steedman <bruce.steedman@gmail.com>
2016-02-16 09:48:14 +0000

    shellchecked

--- justfreevpn.sh
+++ justfreevpn.sh
@@ -8,8 +8,8 @@ for code in "${codes[@]}"
 do
   image_url="$referer"/free-vpn-password/"$code"-free-vpn.gif
   filep=${temp_dir}/${code}
-  wget -q --header="Referer: $referer" -O ${filep}.gif $image_url
-  convert -resample 300 -units PixelsPerInch ${filep}.gif ${filep}.png
-  tesseract ${filep}.png ${filep} > /dev/null 2>&1
-  cat ${filep}.txt # includes extra line break
+  wget -q --header="Referer: $referer" -O "${filep}.gif" "$image_url"
+  convert -resample 300 -units PixelsPerInch "${filep}.gif ${filep}.png"
+  tesseract "${filep}.png" "${filep}" > /dev/null 2>&1
+  cat "${filep}.txt" # includes extra line break
 done
