d08b376ff815f1310a7ddf7601b303ed5d5ca1e1
Bruce Steedman <bruce.steedman@gmail.com>
2016-02-28 00:23:14 +0000

    accepts country code arg

--- justfreevpn.sh
+++ justfreevpn.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 referer="http://justfreevpn.com/"
-declare -a codes=("us" "uk" "ca" "nl" "de" "hk")
+# declare -a codes=("us" "uk" "ca" "nl" "de" "hk")
 temp_dir=$(mktemp -d)
 
-for code in "${codes[@]}"
-do
-  image_url="$referer"/free-vpn-password/"$code"-free-vpn.gif
+# for code in "${codes[@]}"
+# do
+
+  code=$1 # arg is 2 letter contry code
+  image_url="${referer}/free-vpn-password/${code}-free-vpn.gif"
   filep=${temp_dir}/${code}
-  wget -q --header="Referer: $referer" -O "${filep}.gif" "$image_url"
-  convert -resample 300 -units PixelsPerInch "${filep}.gif ${filep}.png"
+  torsocks wget -q --header="Referer: $referer" -O "${filep}.gif" "$image_url" # Tor
+  convert -resample 300 -units PixelsPerInch "${filep}.gif" "${filep}.png"
   tesseract "${filep}.png" "${filep}" > /dev/null 2>&1
   cat "${filep}.txt" # includes extra line break
-done
+# done
